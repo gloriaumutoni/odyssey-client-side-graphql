8af6cb4d00a0c919824590cb12dcd00d74a25153
gloriaumutoni <gloria.umutoni54@gmail.com>
2025-04-11 16:18:16 +0200

    feat: create track query

--- src/pages/tracks.tsx
+++ src/pages/tracks.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { Layout } from "../components";
+import { useQuery, gql } from "@apollo/client";
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+const GET_TRACK = gql(`
+  query GetTracks {
+    tracksForHome {   
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+        }
+        }
+        }
+        `);
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(GET_TRACK);
+  if (error) return `Error! ${error.message}`;
+  if (loading) return "Loading...";
+  return <Layout grid>{JSON.stringify(data)}</Layout>;
 };
 
 export default Tracks;
